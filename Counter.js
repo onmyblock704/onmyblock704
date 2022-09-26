3bde11a21ff1eaa952b183c88a1fff446fae4217
onmyblock704 <114442557+onmyblock704@users.noreply.github.com>
2022-09-26 16:42:21 -0400

    Create Counter.js

---
+++ Counter.js
@@ -0,0 +1,87 @@
+const { expect } = require('chai')
+const { ethers } = require('hardhat');
+
+describe('Counter', () => {
+  let counter
+
+})
+
+
+  beforeEach(async () => {
+    const Counter = await ethers.getContractFactory('Counter')
+   counter = await Counter.deploy('My Counter', 1)
+  
+  })
+
+    describe('Deployment', () => {
+      
+      it('sets the initial count', async () => {
+        expect(await counter.count()).to.equal(1)
+})
+it('sets the initial name', async () => {
+    expect(await counter.name()).to.equal('My Counter')
+
+
+  })
+
+    })
+
+    describe('Counting', () => {
+      let transaction
+
+      it('reads the count from the "count" public variable', async () => {
+        
+        expect(await counter.count()).to.equal(1)
+      })
+
+      it('reads the count from the "getcount()" function', async ( )=> {
+
+        
+        expect(await counter.count()).to.equal(1)
+      })
+
+      it('increments the count', async () =>{
+        let transaction = await  counter.increment()
+         await transaction.wait()
+        
+         expect(await counter.count()).to.equal(2)
+
+        transaction = await  counter.increment()
+         await transaction.wait()
+
+         expect(await counter.count()).to.equal(3) 
+
+      })
+
+
+})
+
+
+      it('decrements the count', async () =>{
+        let transaction = await  counter.decrement()
+         await transaction.wait()
+        
+         expect(await counter.count()).to.equal(0)
+
+        //transaction = await  counter.increment()
+        // await transaction.wait()
+
+         //expect(await counter.count()).to.equal(3)
+})
+
+it('reads the name from the "name" public variable', async () => {
+        
+  expect(await counter.count()).to.equal('My Counter')
+})
+
+it('reads the count from the "getName()" function', async ( )=> {
+
+  
+  expect(await counter.count()).to.equal('My Counter')
+})
+
+it('updates the name ', async () => {
+  transaction = await counter.setName('New Name')
+  await transaction.wait()
+  expect(await counter.name()).to.equal('New Name')
+})
